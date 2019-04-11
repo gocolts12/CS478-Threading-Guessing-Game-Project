28ac350c0b8232d83e8f66530c180eb2c724566e
Michael <mlederer101@gmail.com>
2019-04-11 18:19:24 -0500

    Trying table layout

--- app/src/main/java/com/example/project4/MyTable.java
+++ app/src/main/java/com/example/project4/MyTable.java
@@ -1,6 +1,32 @@
 package com.example.project4;
 
+import android.content.Context;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 public class MyTable extends TableLayout {
+
+    public MyTable(Context context)
+    {
+        super(context);
+//        TableLayout.LayoutParams tableParams = new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT,
+//                TableLayout.LayoutParams.WRAP_CONTENT);
+//        TableRow.LayoutParams rowParams = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT,
+//                TableRow.LayoutParams.WRAP_CONTENT);
+//
+//        TableLayout tableLayout = new TableLayout(context);
+//        tableLayout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+//                LinearLayout.LayoutParams.WRAP_CONTENT));// assuming the parent view is a LinearLayout
+//
+//        TableRow tableRow = new TableRow(context);
+//        tableRow.setLayoutParams(tableParams);// TableLayout is the parent view
+//
+//        ImageView img = new ImageView(context);
+//        img.setLayoutParams(rowParams);// TableRow is the parent view
+//
+//        tableRow.addView(img);
+    }
 }
